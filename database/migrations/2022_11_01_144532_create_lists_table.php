e5695ad1211f13ed679fa2825dec7c465d33270a
Djordje Arsenovic <dj.arsenovic@gmail.com>
2022-11-01 15:58:21 +0100

    Created migrations, models, controllers for the task lists and tasks

---
+++ database/migrations/2022_11_01_144532_create_lists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('lists', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('subtitle');
+      $table->string('background');
+      $table->string('thumbnail');
+      $table->text('description');
+      $table->foreignId('user_id')->references('id')->on('users');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('lists');
+  }
+}
