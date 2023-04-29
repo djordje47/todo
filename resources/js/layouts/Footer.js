e353166c9df5246d461224a5f620c1a66957505e
Djordje Arsenovic <dj.arsenovic@gmail.com>
2023-04-29 22:12:38 +0200

    added footer

--- resources/js/layouts/Footer.js
+++ resources/js/layouts/Footer.js
@@ -5,8 +5,9 @@ function Footer() {
       <footer className="bottom-0 position-absolute w-100">
         <div className="container-fluid">
           <div className="row justify-content-center align-content-center align-items-center">
-            <div className="col-12">
-              <p className="text-center">All rights reserved @ Djordje Arsenovic</p>
+            <div className="col-12 text-center">
+              <p className="d-inline">All rights reserved Djordje Arsenovic @ </p> <a
+                href="https://djolecodes.com" target="_blank">{'<djolecodes.com />'}</a>
             </div>
           </div>
         </div>
